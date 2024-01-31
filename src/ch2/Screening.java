c9b4f56c21c437d4b0a158e4ac3f079eeffa2280
MinseokGo <rhalstjr1999@naver.com>
2024-01-31 18:49:01 +0900

    feat: movie reservation system

---
+++ src/ch2/Screening.java
@@ -0,0 +1,36 @@
+package ch2;
+
+import java.time.LocalDateTime;
+
+public class Screening {
+    private final Movie movie;
+    private final int sequence;
+    private final LocalDateTime whenScreened;
+
+    public Screening(final Movie movie, final int sequence, final LocalDateTime whenScreened) {
+        this.movie = movie;
+        this.sequence = sequence;
+        this.whenScreened = whenScreened;
+    }
+
+    public Reservation reserve(final Customer customer, final int audienceCount) {
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
+    }
+
+    private Money calculateFee(final int audienceCount) {
+        return movie.calculateMovieFee(this)
+                .times(audienceCount);
+    }
+
+    public LocalDateTime getStartTime() {
+        return whenScreened;
+    }
+
+    public boolean isSequence(final int sequence) {
+        return this.sequence == sequence;
+    }
+
+    public Money getMovieFee() {
+        return movie.getFee();
+    }
+}
