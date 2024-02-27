f0958e765d967c4ef1a539366b72ab8f4418e059
MinseokGo <rhalstjr1999@naver.com>
2024-02-27 23:01:59 +0900

    feat: reservation and customer

---
+++ src/ch4/Customer.java
@@ -0,0 +1,11 @@
+package ch4;
+
+public class Customer {
+    private final String name;
+    private final String id;
+
+    public Customer(final String name, final String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
