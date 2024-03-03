ca38d24754f6bae56b665b1726f0928d86986777
MinseokGo <rhalstjr1999@naver.com>
2024-03-03 18:31:45 +0900

    refactor: condition relation is moved in condition object

--- src/ch4/discount/DiscountCondition.java
+++ src/ch4/discount/DiscountCondition.java
@@ -12,6 +12,24 @@ public class DiscountCondition {
     private LocalTime startTime;
     private LocalTime endTime;
 
+    public boolean isDiscountable(final DayOfWeek dayOfWeek, final LocalTime time) {
+        if (type != DiscountConditionType.PERIOD) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.dayOfWeek.equals(dayOfWeek) &&
+                this.startTime.isBefore(time) &&
+                this.endTime.isAfter(time);
+    }
+
+    public boolean isDiscountable(final int sequence) {
+        if (type != DiscountConditionType.SEQUENCE) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.sequence == sequence;
+    }
+
     public DiscountConditionType getType() {
         return type;
     }
